17e042f72afda2fbe26bc0acb5d5fe117b8e18ac
Rodrigo <rofen1@protonmail.com>
2025-02-08 17:59:49 -0300

    feat: Configurar docker e api

---
+++ backend/src/main/java/com/br/ps/esig/ps_esig/PsEsigApplication.java
@@ -0,0 +1,11 @@
+package com.br.ps.esig.ps_esig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsEsigApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PsEsigApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/br/ps/esig/ps_esig/PsEsigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ps.esig.ps_esig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsEsigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
