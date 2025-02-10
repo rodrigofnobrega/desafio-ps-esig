8e60b3546d69e5a1a1dba3153539b68aeaeef425
Rodrigo <rofen1@protonmail.com>
2025-02-09 23:11:43 -0300

    fix: Corrigir formato das datas no dto de resposta das tarefas

--- backend/src/main/java/com/br/ps/esig/ps_esig/dto/task/TaskResponseDTO.java
+++ backend/src/main/java/com/br/ps/esig/ps_esig/dto/task/TaskResponseDTO.java
@@ -2,6 +2,9 @@ package com.br.ps.esig.ps_esig.dto.task;
 
 import com.br.ps.esig.ps_esig.enums.PriorityEnum;
 import com.br.ps.esig.ps_esig.enums.TaskSituationEnum;
+import com.br.ps.esig.ps_esig.utils.RefactorDate;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -18,7 +21,25 @@ public class TaskResponseDTO {
     private String description;
     private TaskSituationEnum taskSituation;
     private PriorityEnum priority;
+    @JsonIgnore
     private LocalDate date;
+    @JsonIgnore
     private LocalDateTime createdAt;
+    @JsonIgnore
     private LocalDateTime updatedAt;
+
+    @JsonProperty("createdAt")
+    private String getCreateAtFormatted() {
+        return RefactorDate.refactorLocalDateTime(getCreatedAt());
+    }
+
+    @JsonProperty("updatedAt")
+    private String getUpdatedAtFormatted() {
+        return RefactorDate.refactorLocalDateTime(getUpdatedAt());
+    }
+
+    @JsonProperty("date")
+    private String getDateFormatted() {
+        return RefactorDate.refactorLocalDate(getDate());
+    }
 }
